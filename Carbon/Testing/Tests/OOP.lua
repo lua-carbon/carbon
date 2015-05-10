0e524ff625c6424c0377c512960710eb99ce88ae
Lucien Greathouse <me@lpghatguy.com>
2015-05-10 00:32:29 -0600

    Full basic OOP coverage

--- Carbon/Testing/Tests/OOP.lua
+++ Carbon/Testing/Tests/OOP.lua
@@ -17,27 +17,70 @@ function Test:Run(test)
 	)
 
 	local A = OOP:Class()
+		:Members {
+			Hello = "World"
+		}
 
 	test:Assert(
 		A.Inherits and A.Attributes and A.Metatable and A.Members,
-		"Class failed to have basic creation methods!"
+		"Class A failed to have basic creation methods!"
 	)
 
 	test:Assert(
 		A.Init and A.New and A.PlacementNew and A.Is,
-		"Class failed to have basic usage methods!"
+		"Class A failed to have basic usage methods!"
+	)
+
+	test:Assert(
+		A.Hello == "World",
+		"Class A failed to have property!"
 	)
 
 	local Ai = A:New()
 
 	test:Assert(
 		Ai.Init and Ai.Copy and Ai.Is,
-		"Instance failed to have basic usage methods!"
+		"Instance A failed to have basic usage methods!"
 	)
 
 	test:Assert(
-		Ai.Is[A],
-		"Instance failed to have proper Is!"
+		Ai.Is[OOP.Object] and Ai.Is[A],
+		"Instance A failed to have proper Is!"
+	)
+
+	test:Assert(
+		Ai.Hello == "World",
+		"Instance A failed to have correct property!"
+	)
+
+	local B = OOP:Class()
+		:Inherits(A)
+		:Members {
+			Hello = "Earth"
+		}
+
+	local Bi = B:New()
+
+	test:Assert(
+		Bi.Is[OOP.Object] and Bi.Is[A] and Bi.Is[B],
+		"Instance B failed to have proper Is!"
+	)
+
+	-- Test multiple inheritance
+	local C = OOP:Class()
+		:Inherits(A, B)
+
+	local Ci = C:New()
+
+	test:Assert(
+		Ci.Is[OOP.Object] and Ci.Is[A] and Ci.Is[B] and Ci.Is[C],
+		"Instance C (MI) failed to have property Is!"
+	)
+
+	-- Property inheritance is first-come-first-serve
+	test:Assert(
+		Ci.Hello == "World",
+		"Instance C failed to take on defined property inheriance!"
 	)
 end
 
